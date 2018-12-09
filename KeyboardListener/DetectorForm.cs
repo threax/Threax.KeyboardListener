cd59822e7eeb90e2ca4993661589375bc7b38fca
Andrew Piper <andrew@threax.com>
2018-12-09 06:47:45 -0500

    Cut down to just basics.

--- KeyboardListener/DetectorForm.cs
+++ KeyboardListener/DetectorForm.cs
@@ -45,26 +45,7 @@ namespace SleepDetector
             RegisterHotKey(this.Handle, TvEvent, 0x0001 | 0x0002 | 0x4000, (int)Keys.T);
             RegisterHotKey(this.Handle, SoundEvent, 0x0001 | 0x0002 | 0x4000, (int)Keys.S);
             RegisterHotKey(this.Handle, SystemEvent, 0x0001 | 0x0002 | 0x4000, (int)Keys.P);
-
-            ToggleProgram = "toggle.bat";
-            SleepProgram = "sleep.bat";
-            WakeProgram = "wake.bat";
-            SystemProgram = "system.bat";
-            TvProgram = "tv.bat";
-            SoundProgram = "sound.bat";
         }
-
-        public String ToggleProgram { get; set; }
-
-        public String SleepProgram { get; set; }
-
-        public String WakeProgram { get; set; }
-
-        public String SystemProgram { get; set; }
-
-        public String TvProgram { get; set; }
-
-        public String SoundProgram { get; set; }
 
         public bool SleepMode
         {
@@ -78,78 +59,31 @@ namespace SleepDetector
             }
         }
 
+        private void HotKeyEvent(long evt)
+        {
+            Console.WriteLine(evt);
+        }
+
         protected override void WndProc(ref Message m)
         {
             switch (m.Msg)
             {
-                case WM_POWERBROADCAST:
-                    switch (m.WParam.ToInt64())
-                    {
-                        case PBT_APMSUSPEND:
-                            break;
-                        case PBT_APMRESUMESUSPEND:
-                            runProgram(WakeProgram, false);
-                            break;
-                    }
-                    break;
+                //This is wake
+                //case WM_POWERBROADCAST:
+                //    switch (m.WParam.ToInt64())
+                //    {
+                //        case PBT_APMSUSPEND:
+                //            break;
+                //        case PBT_APMRESUMESUSPEND:
+                //            runProgram(WakeProgram, false);
+                //            break;
+                //    }
+                //    break;
                 case WM_HOTKEY:
-                    switch (m.WParam.ToInt64())
-                    {
-                        case SleepEvent:
-                            if (sleepMode)
-                            {
-                                runProgram(SleepProgram, true);
-                                Application.SetSuspendState(PowerState.Suspend, true, true);
-                            }
-                            else
-                            {
-                                runProgram(ToggleProgram, false);
-                            }
-                            break;
-                        case TvEvent:
-                            runProgram(TvProgram, false);
-                            break;
-                        case SoundEvent:
-                            runProgram(SoundProgram, false);
-                            break;
-                        case SystemEvent:
-                            runProgram(SystemProgram, false);
-                            break;
-                    }
+                    HotKeyEvent(m.WParam.ToInt64());
                     break;
             }
             base.WndProc(ref m);
-        }
-
-        private void runProgram(String program, bool waitForExit)
-        {
-            Console.WriteLine(program);
-            //if (!String.IsNullOrEmpty(program))
-            //{
-            //    if (File.Exists(program))
-            //    {
-            //        try
-            //        {
-            //            var startInfo = new ProcessStartInfo();
-            //            startInfo.FileName = program;
-            //            if (!showWindowsToolStripMenuItem.Checked)
-            //            {
-            //                startInfo.CreateNoWindow = true;
-            //                startInfo.UseShellExecute = false;
-            //            }
-            //            var process = Process.Start(startInfo);
-            //            if (waitForExit)
-            //            {
-            //                process.WaitForExit();
-            //            }
-            //        }
-            //        catch (Exception) { }
-            //    }
-            //    else if(showWindowsToolStripMenuItem.Checked)
-            //    {
-            //        MessageBox.Show(String.Format("Cannot find program '{0}'", program), "Progam not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    }
-            //}
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
