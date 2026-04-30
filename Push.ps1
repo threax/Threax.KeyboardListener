b1988f484f020012a41c3107545259332d61e746
Andrew Piper <andrew@threax.com>
2026-04-30 19:43:00 -0400

    Add push script.

---
+++ Push.ps1
@@ -0,0 +1,4 @@
+git push
+git push https://github.com/threax/Threax.KeyboardListener.git
+git push https://gitlab.com/threax-software/Threax.KeyboardListener.git
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.KeyboardListener
